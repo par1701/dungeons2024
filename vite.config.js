aaad9c29bcae007f7977d329750f71f4c8aa87da
Ignacio <pardonner@gmail.com>
2026-05-31 21:21:22 +0200

    added files

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Cambia "dnd-sheet" por el nombre exacto de tu repositorio en GitHub
+export default defineConfig({
+  plugins: [react()],
+  base: "/dnd-sheet/",
+});
